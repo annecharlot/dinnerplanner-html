0c939d4805a10fc978332e3495ca27c43b5f05dd
annecharlot <acscholz@kth.se>
2019-01-23 15:51:38 +0100

    dinner

--- js/view/dishdetailsview.js
+++ js/view/dishdetailsview.js
@@ -3,20 +3,37 @@ var DetailView = function (container, model) {
 
 	var ingredientlist = container.find("#ingredients");
 	var item = container.find("#item");
-	var numberguests = container.find("#numberOfGuests");
-
+	
 	var dsh = model.getDish(1);
 
 	var img = "///Users/annecharlot/Documents/KTH/ÅK3/interaktionsprogrammering/GitHub/dinnerplanner-html/images/" + dsh.image;
 
-	var source= "<h2>" + dsh.name + "</h2>" + "<img src='" + img + "'style='outline: 1px solid black;'/>"; 
+	var source= "<h2>" + dsh.name + "</h2>" + "<img src='" + img + "'style='outline: 1px solid black;'/>"
+	+ "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>"
+    + "<button class='btn' style='background-color: rgba(244, 179, 80, 1); border: black; box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);''>"
+    + "back to search</button>"
+    + "<h3>Preparation</h3>"
+    + "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>";
 
-	model.setNumberOfGuests(3);
-	var number = model.getNumberOfGuests();
+    item.html(source);
+    
+	var ing = dsh.ingredients;
 
-	item.html(source);
-	ingredientlist.html("Hello World!");
-	numberguests.html(number);
+	for (x of ing) {
+		var div = "<div class='row'><div class='col-md-2 col-xs-4'>"
+		+ ing.quantity + ing.unit + "</div>"
+	+ "<div class='col-md-2 col-xs-4'>" + ing.name + "</div><div class='col-md-2 col-xs-4'>SEK</div>"
+	+ "<div class='col-md-2 col-xs-4'>"
+	+ ing.price + "</div></div>";
+	ingredientlist.html(div);
+
+
+	}
+
+	
+	
+	
+
 	
 }
  
