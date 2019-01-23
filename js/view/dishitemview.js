a76832de220e9f464741645fc7301703ebf8b978
annecharlot <acscholz@kth.se>
2019-01-23 16:14:27 +0100

    dinner

--- js/view/dishitemview.js
+++ js/view/dishitemview.js
@@ -5,6 +5,7 @@ var ItemView = function (container, model) {
 	var dishimg2 = container.find("#dishimg2");
 	var dishname = container.find("#dishname");
 	var dishname2 = container.find("#dishname2");
+	var fullitem = container.find("#fullitem");
 
 	/** model.addDishToMenu(202);
 	model.addDishToMenu(1);
@@ -23,10 +24,18 @@ var ItemView = function (container, model) {
 	var item = "<div>" + source + "</div>";
 
 	var text="<h3>" + dsh.name + "</h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+	
+	var full = "<div class='col-xs-2 col-md-2' style='padding: 5px;'>" + source + "</div>"
+	+ "<div class='col-xs-5 col-md-5'>" + text + "</div>"
+	+ "<div class='col-xs-5 col-md-5'><h4>Preparation</h4>"
+	+ "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+	+ "</div><br/>";
 
-	dishimg.html(item);
+	fullitem.html(full);
+
+	
+
 	dishimg2.html(source2);
-	dishname.html(text);
 	dishname2.html(dsh2.name);
 
 }
