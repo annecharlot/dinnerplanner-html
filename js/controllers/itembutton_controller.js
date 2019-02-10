6dcdcc07c6649edb64533aa3107bb3f29c0949a1
annecharlot <acscholz@kth.se>
2019-02-10 18:44:01 +0100

    Update itembutton_controller.js

--- js/controllers/itembutton_controller.js
+++ js/controllers/itembutton_controller.js
@@ -1,5 +1,5 @@
 var ItemButtonController = function(itembutton, id, app /**, id = dsh.id som vi klicka på */) {
-    console.log(itembutton);
+    
     itembutton.addEventListener("click", function(){
 
        app.dishdetails(id);
