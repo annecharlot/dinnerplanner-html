ba1144acf4c7c5e733fd02494e8e8097f6690a2c
annecharlot <acscholz@kth.se>
2019-02-14 09:49:25 +0100

    Update dishdetailsview.js

--- js/view/dishdetailsview.js
+++ js/view/dishdetailsview.js
@@ -11,41 +11,10 @@ var DetailView = function (container, model) {
 	this.update = function(args){
 
 	if (args === 'NumberOfGuests'){
-			var guests = container.find("#number");
-			var ingredientlist = container.find("#ingredients");
-			var totalprice = container.find("#totalprice");
-
-			var number = model.getNumberOfGuests();
-
-			//Du får inte göra detta :(( ropa inte på getDish()
-			model.getDish(this.id).then(data => {
-		
-			var dsh = data;
-
-			var list = dsh.extendedIngredients;
-
-			var div = "";
-			var price = 0;
-
-			for (let ing of list) {
-				div += "<div class='row'><div class='col-md-4 col-xs-4'>" + Math.round(ing.measures.metric.amount*number) + " " + ing.measures.metric.unitShort + "</div>"
-			+ "<div class='col-md-4 col-xs-4'>" + ing.originalName + "</div>"
-			+ "<div class='col-md-4 col-xs-4'>SEK "
-			+ number + ".00</div></div>";
-
-			ingredientlist.html(div);
-
-
-			};
-
-			totalprice.html(Math.round(dsh.pricePerServing*number));
-			guests.html(number);
-
-		}).catch( error => {
-		});
-
+			this.updateingredients();
 
 	}
+	
 	else {
 
 		var ingredientlist = container.find("#ingredients");
@@ -70,14 +39,14 @@ var DetailView = function (container, model) {
 
 		model.getDish(this.id).then(data => {
 		
-			var dsh = data;
+			this.dsh = data;
 
-			var source= "<h2>" + dsh.title + "</h2>" + "<img src='" + dsh.image + "'style='outline: 1px solid black;'/>";
+			var source= "<h2>" + this.dsh.title + "</h2>" + "<img src='" + this.dsh.image + "'style='outline: 1px solid black;'/>";
 
 		    item.html(source);
 		   
 
-			var list = dsh.extendedIngredients;
+			var list = this.dsh.extendedIngredients;
 
 			var div = "";
 			var price = 0;
@@ -95,16 +64,16 @@ var DetailView = function (container, model) {
 
 			};
 
-			totalprice.html(Math.round(dsh.pricePerServing*number));
+			totalprice.html(Math.round(this.dsh.pricePerServing*number));
 			guests.html(number);
 
 
 		   	var Preparation = "<h3>Preparation</h3>"
-		   	+ dsh.instructions;
+		   	+ this.dsh.instructions;
 
 		   	preparation.html(Preparation);
 
-		   	this.dsh_id = dsh.id;
+		   	this.dsh_id = this.dsh.id;
 
 		   	loader.hide();
 
@@ -121,6 +90,34 @@ var DetailView = function (container, model) {
 		this.addbutton = container.find("#add")
 		}
 
+		//Re-renders the ingredient window only
+		this.updateingredients = function() {
+			var guests = container.find("#number");
+			var ingredientlist = container.find("#ingredients");
+			var totalprice = container.find("#totalprice");
+
+			var number = model.getNumberOfGuests();			
+
+			var list = this.dsh.extendedIngredients;
+
+			var div = "";
+			var price = 0;
+
+			for (let ing of list) {
+				div += "<div class='row'><div class='col-md-4 col-xs-4'>" + Math.round(ing.measures.metric.amount*number) + " " + ing.measures.metric.unitShort + "</div>"
+			+ "<div class='col-md-4 col-xs-4'>" + ing.originalName + "</div>"
+			+ "<div class='col-md-4 col-xs-4'>SEK "
+			+ number + ".00</div></div>";
+
+			ingredientlist.html(div);
+
+
+			};
+
+			totalprice.html(Math.round(this.dsh.pricePerServing*number));
+			guests.html(number);
+
+		}
 	}
 
 this.update();
